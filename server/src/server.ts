ded9fa7f60a91fd12f176c0eee0dbe2f95c793c3
Devyn Johnson <dljohnson1313@gmail.com>
2025-03-18 20:03:39 -0500

    Missed a ../ on server.ts, Render is attempting to serve server/client/dist/index.html. Fixed, will attempt to redeploy.

--- server/src/server.ts
+++ server/src/server.ts
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 }
 );
 
